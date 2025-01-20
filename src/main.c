44948645036120a03b0a72d54a12bf06c255a020
RafaelRSoares <69215558+RafaelRSoares@users.noreply.github.com>
2025-01-20 15:31:20 -0300

    Update main.c
    Coloquei aqui o if pra definir qual o algortimo e o formato de como as coisas serão impressas, só falta agora adicionar dois contadores nos algoritmos de ordenação, um para movimentações e um para comparações, e puxar eles aqui para serem impressos. Acho que colocar os algortimos como função int e colocar esses contadores como saída pode resolver isso.

--- src/main.c
+++ src/main.c
@@ -9,40 +9,79 @@ int main() {
     clock_t start, end;
     double cpu_time_used;
 
-    start = clock(); //início da medição
+    printf("\nQual algoritmo deseja usar parar ordenar o conjunto de rochas, simples(1) ou sofisticado(2)?\n")
+    int resposta;
+    scanf("%d, &resposta");
 
-    //lista encadeada para armazenar as rochas
-    Compartimento rochas;
-    FListaRochaM(&rochas);
+    if (resposta == 1){
+        start = clock(); //início da medição
 
-    int quantidade_rochas;
-
-    //carrega as rochas a partir do arquivo
-    quantidade_rochas = LerArquivo("entrada_teste.txt", &rochas);
+        //lista encadeada para armazenar as rochas
+        Compartimento rochas;
+        FListaRochaM(&rochas);
     
-    RochaMineral *vetor_auxiliar = malloc(quantidade_rochas * sizeof(RochaMineral));
+        int quantidade_rochas;
+    
+        //carrega as rochas a partir do arquivo
+        quantidade_rochas = LerArquivo("entrada_teste.txt", &rochas);
+        
+        RochaMineral *vetor_auxiliar = malloc(quantidade_rochas * sizeof(RochaMineral));
+    
+        vetor_aux(&rochas, vetor_auxiliar, quantidade_rochas);
+    
+        ImprimePesosListaRochaM(&rochas);
+    
+        printf("Metodo de Organizacao: SelectionSort\n");
+        selecao(vetor_auxiliar, quantidade_rochas);
+        for(int i = 0; i < quantidade_rochas; i++){
+            printf("%.2f ", vetor_auxiliar[i].peso);
+        }
+    
+        free(vetor_auxiliar);
+    
+        end = clock(); //fim da medição
+        void ImprimeListaRochaM(Compartimento *rochas);
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; //tempo em segundos
+        printf("\nComparações: a ser concluído");
+        printf("\nMovimentações: a ser concluído");
+        printf("\nTempo de execucao: %.3f segundos", cpu_time_used);
+        printf("\nAlgoritmo: SelectionSort");
+        
+        return 0;
+    }
+    else if (resposta == 2){
+        start = clock(); //início da medição
 
-    vetor_aux(&rochas, vetor_auxiliar, quantidade_rochas);
-
-    ImprimePesosListaRochaM(&rochas);
-
-    printf("Metodo de Organizacao: SelectionSort\n");
-    selecao(vetor_auxiliar, quantidade_rochas);
-    for(int i = 0; i < quantidade_rochas; i++){
-        printf("%.2f ", vetor_auxiliar[i].peso);
+        Compartimento rochas;
+        FListaRochaM(&rochas);
+    
+        int quantidade_rochas;
+    
+        quantidade_rochas = LerArquivo("entrada_teste.txt", &rochas);
+        
+        RochaMineral *vetor_auxiliar = malloc(quantidade_rochas * sizeof(RochaMineral));
+    
+        vetor_aux(&rochas, vetor_auxiliar, quantidade_rochas);
+    
+        ImprimePesosListaRochaM(&rochas);
+        
+        printf("Metodo de Organizacao: ShellSort\n");
+        shellsort(vetor_auxiliar, quantidade_rochas);
+        for(int i = 0; i < quantidade_rochas; i++){
+            printf("%f", vetor_auxiliar[i].peso);
+        }
+        free(vetor_auxiliar);
+    
+        end = clock();
+    
+        void ImprimeListaRochaM(Compartimento *rochas);
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; //tempo em segundos
+        printf("\nComparações: a ser feito");
+        printf("\nMovimentações: a ser feito");
+        printf("\nTempo de execucao: %.3f segundos", cpu_time_used);
+        printf("\nAlgoritmo: ShellSort");
+        return 0;
     }
 
-    // printf("Metodo de Organizacao: ShellSort\n");
-    // shellsort(vetor_auxiliar, quantidade_rochas);
-    // for(int i = 0; i < quantidade_rochas; i++){
-    //     printf("%f", vetor_auxiliar[i].peso);
-    // }
-
-    free(vetor_auxiliar);
-
-    end = clock(); //fim da medição
-
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC; //tempo em segundos
-    printf("Tempo de execucao: %.3f segundos\n", cpu_time_used);
-    return 0;
-}
+    
+   
